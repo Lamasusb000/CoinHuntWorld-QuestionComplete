0d0ff8ecac7afa25f4739e04aeb8a46078daeded
Lamasusb000 <71854221+Lamasusb000@users.noreply.github.com>
2021-07-10 23:51:35 -0700

    Removed Markdown stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,13 +8,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/src/markdown-pages`
-      }
-    },
-    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "page-settings",
--- src/markdown-pages/AboutThisSite.md
+++
@@ -1,8 +0,0 @@
----
-slug: About-This-Site
-date: 2021-07-9T03:30:51.473Z
-title: About This Site
-subtitle: -Junior (MrTurtlesGame)
----
-
--This is a **Work in Progress**
