86cd92d315f47d3466e441c170bca656467e5594
Lamasusb000 <usbaldolamasjr@gmail.com>
2021-07-13 11:29:13 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import ImportJS from "../components/JS-Import"
 
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
+    <ImportJS File="JSONfileUpload.js"/>
   </Layout>
 )
 
