6ae649724240dc9a1aa0fe3b574bb96ff192d9ec
Lamasusb000 <71854221+Lamasusb000@users.noreply.github.com>
2021-07-13 16:33:21 -0700

    New functions

---
+++ functions/SelectedQuestions-Category.js
@@ -0,0 +1,30 @@
+const faunadb = require('faunadb'),
+q = faunadb.query;
+const Client = new faunadb.Client({ secret: 'fnAEN56_MwACQKzzE9wDEAAY4w5EUN7nNnstIyAN' })
+var Output = "JSON Did not Attach"
+
+exports.handler = (event, context, callback) => {
+	Client.query(
+		q.Paginate(
+			q.Match(
+				q.Index('VaultColorSelection'),
+				 `${event.body}`
+			)
+		)
+	)
+	.then(function(result){
+		if(result == "" | undefined){
+			console.log("No Result")
+		}
+        var Questions = []
+		Output = result.data
+        for (let i = 0; i < Output.length; i++) {
+            Questions[i] = Output[i].data
+            
+        }
+		return callback(null, {
+			statusCode: 200,
+			body: `${JSON.stringify(Questions)}`
+		  })
+	})
+}
