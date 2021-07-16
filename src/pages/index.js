f742c3c01910f5893c023bacf7502d4cace89a01
Lamasusb000 <usbaldolamasjr@gmail.com>
2021-07-15 20:39:08 -0700

    Road map v1

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,34 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <ImportJS File="JSONfileUpload.js"/>
-	<img src="https://api.netlify.com/api/v1/badges/ff689e6b-7919-45be-aea5-69df18e249cf/deploy-status" alt="Deploy status badge"></img>
+    <h3>
+      Road Map
+    </h3>
+    <ol>
+      <li>
+        Integrate the croppie.js library into the submission box to allow prompts of "Select Category/Question/Answer"
+      </li>
+      <li>
+        Write JS to handle multiple OCR submissions and collections
+      </li>
+      <li>
+        Write JS that will talk to self hosted API that will pass along information to the database
+      </li>
+      <li>
+        Impliment Netlify Idendity to get users to create an account and get a Account-ID hash that will be used to track user activity (Prevent Malicious Intents)
+      </li>
+      <li>
+        Add Search Box Filtering by Color/Category
+        <ul>
+          <li>
+            Just needs to get HTML, CSS and JS written. Database already has Indexes implimented
+          </li>
+        </ul>
+      </li>
+    </ol>
+    <br/><br/>
+    <p>Site Deployment Status</p>
+	  <img src="https://api.netlify.com/api/v1/badges/ff689e6b-7919-45be-aea5-69df18e249cf/deploy-status" alt="Deploy status badge"></img>
   </Layout>
 )
 
