5d162e595a219437074fa03b8a7d570a3e3540fd
Lamasusb000 <usbaldolamasjr@gmail.com>
2021-07-25 10:13:35 -0700

    Auto Reattempting Beta Checker

--- static/JS/BetaTesting.js
+++ static/JS/BetaTesting.js
@@ -1,16 +1,19 @@
-$("#NetlifyIdentity").off("load", BetaV2)
-$("#NetlifyIdentity").on("load", BetaV2)
-
 function BetaV2(){
-    if (netlifyIdentity.currentUser()){
-        if (netlifyIdentity.currentUser().app_metadata.roles.includes("Beta")){
-            console.log("Approved Tester")
+    try{
+        if (netlifyIdentity.currentUser()){
+            if (netlifyIdentity.currentUser().app_metadata.roles.includes("Beta")){
+                console.log("Approved Tester")
+            }else{
+                console.log("Not an Approved Tester")
+                window.location.href = "/"
+            }
         }else{
             console.log("Not an Approved Tester")
             window.location.href = "/"
         }
-    }else{
-        console.log("Not an Approved Tester")
-        window.location.href = "/"
+    }catch(err){
+        setTimeout(BetaV2, 100)
     }
 }
+
+BetaV2
