170116cb522d47c55ab319e3d360795da44fc519
Lamasusb000 <usbaldolamasjr@gmail.com>
2021-07-25 08:56:47 -0700

    Beta testing update

--- static/JS/BetaTesting.js
+++ static/JS/BetaTesting.js
@@ -1,6 +1,11 @@
-if (netlifyIdentity.currentUser()){
-    console.log("Approved Tester")
-}else{
-    console.log("Not An Approved Tester")
-    window.location.href = "/"
+$("#NetlifyIdentity").off("load", Beta)
+$("#NetlifyIdentity").on("load", Beta)
+
+function Beta(){
+    if (netlifyIdentity.currentUser()){
+        console.log("Approved Tester")
+    }else{
+        console.log("Not An Approved Tester")
+        window.location.href = "/"
+    }
 }
