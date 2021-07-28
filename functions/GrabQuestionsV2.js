22a615ee28de3aa53b2d7eaa9f3aadfc8420179f
Lamasusb000 <usbaldolamasjr@gmail.com>
2021-07-28 13:30:50 -0700

    Added Redirect rules

--- functions/GrabQuestionsV2.js
+++ functions/GrabQuestionsV2.js
@@ -4,7 +4,12 @@ const Client = new faunadb.Client({ secret: 'fnAEN56_MwACQKzzE9wDEAAY4w5EUN7nNns
 var Output = "JSON Did not Attach"
 
 exports.handler = (event, context, callback) => {
-    console.log(event.headers.origin)
+    if(event.headers.origin != "https://coinhuntworldtrivia.com"){
+		return callback(null, {
+			statusCode: 403,
+			body: "Sorry But This API is For CoinHuntWorldTrivia.com. Please Reach To Arrange Access"
+		})
+	}
 	Client.query(
 		q.Paginate(
 			q.Match(
