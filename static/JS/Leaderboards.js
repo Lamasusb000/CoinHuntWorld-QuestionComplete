2b3610051ae1a1956d66eb6cb00ad4cffa8d43e4
Lamasusb000 <usbaldolamasjr@gmail.com>
2021-08-03 13:10:36 -0700

    Update Leaderboards.js

--- static/JS/Leaderboards.js
+++ static/JS/Leaderboards.js
@@ -161,3 +161,15 @@ if(window.LeaderboardPreventor == undefined){
 	window.LeaderboardPreventor = true
 	LoadLeaderboards()
 }
+
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
