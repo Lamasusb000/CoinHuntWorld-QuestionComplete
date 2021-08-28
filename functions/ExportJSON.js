ac5c50fa5f12d2664d056d0c413826130fc9bd36
Lamasusb000 <usbaldolamasjr@gmail.com>
2021-08-27 19:10:04 -0700

    Update ExportJSON.js

--- functions/ExportJSON.js
+++ functions/ExportJSON.js
@@ -1,24 +1,25 @@
-const faunadb = require('faunadb'),
-q = faunadb.query;
-const Client = new faunadb.Client({ secret: 'fnAEN56_MwACQKzzE9wDEAAY4w5EUN7nNnstIyAN' })
+const faunadb = require("faunadb"),
+    q = faunadb.query
+const Client = new faunadb.Client({
+    secret: "fnAEN56_MwACQKzzE9wDEAAY4w5EUN7nNnstIyAN",
+})
 var Output = "JSON Did not Attach"
 
 exports.handler = (event, context, callback) => {
     console.log(event.body)
-	Client.query(
-		q.Paginate(
-			q.Match(
-				q.Index('ExportJSON')),
-				{size: 10000}
-		)
-	)
-	.then(function(result){
-		if(result == "" | undefined){
-			console.log("No Result")
-		}
-		return callback(null, {
-			statusCode: 200,
-			body: `${JSON.stringify(result.data)}`
-		  })
-	})
-  }
+    Client.query(
+        q.Paginate(q.Match(q.Index("ExportJSON")), { size: 10000 })
+    ).then(function (result) {
+        if ((result == "") | undefined) {
+            console.log("No Result")
+        }
+        return callback(null, {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true,
+            },
+            body: `${JSON.stringify(result.data)}`,
+        })
+    })
+}
