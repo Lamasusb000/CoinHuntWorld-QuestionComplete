4815a205c9092765a17cb04b198ff6a7da8b35f4
Lamasusb000 <usbaldolamasjr@gmail.com>
2021-09-04 22:17:01 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,24 +22,19 @@ function IndexPage() {
                     Coded For The Community By MrTurtlesGame
                 </p>
                 <hr />
-                <p className="Indent2">
-                    This site is a community created tool to be used to assist
-                    in your adventures on the CoinHuntWorld App! CoinHuntWorld
-                    is a geolocation game where users travel around the real
-                    world to places around their cities to collect keys and
-                    answer questions to collect REAL Crypto!
-                </p>
-                <p className="Indent2">
-                    Sometimes Within the game hunters recieve questions that are
-                    so bizzare even Google can't help them solve it but with a
-                    community sourced database hosted here any hunter can help
-                    their fellow hunters solve their most complex questions with
-                    a simple search from our database!
-                </p>
-                <p className="Indent2">
-                    Remember! Cheating is not fun and please be safe hunting the
-                    crypto and most of all... Have fun!
-                </p>
+                <p>Current Known Bugs</p>
+                <ul>
+                    <li>
+                        Database Does Not Properly Sync While on Flashcards,
+                        Index Page
+                    </li>
+                    <ul>
+                        <li>
+                            Temporary Solution. On The Search Page Press
+                            "Refresh Database Manually"
+                        </li>
+                    </ul>
+                </ul>
             </div>
             <br />
             <br />
