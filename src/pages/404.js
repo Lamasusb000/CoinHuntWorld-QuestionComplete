fc40cc56d50b340b22d6eec9ff3f50aba7548fe6
Lamasusb000 <usbaldolamasjr@gmail.com>
2021-10-06 16:50:26 -0700

    Funny 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,16 +2,57 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import BlueVault from "../images/VaultBlue.png"
+import HappyCubie from "../images/HappyCubie.png"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
     <Layout>
         <Seo title="404: Not found" />
-        <h1>Sorry There Bud</h1>
-        <h3>
-            The Page You Tried To Find Does Not Exist
-            <br />
-            Just Tweet Me Angry and I'll See What's Wrong
-        </h3>
+        <div className="text-center">
+            <h1>Seems like you got lost</h1>
+            <p>While you're here lets enjoy a Disco Party</p>
+        </div>
+        <div className="container DiscoContainer">
+            <div className="row">
+                <div className="col">
+                    <img className="Disco" src={BlueVault} />
+                </div>
+                <div className="col">
+                    <img className="Disco" src={BlueVault} />
+                </div>
+                <div className="col">
+                    <img className="Disco" src={BlueVault} />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <img className="Disco" src={BlueVault} />
+                </div>
+                <div className="col DiscoDance">
+                    <img src={HappyCubie} />
+                </div>
+                <div className="col">
+                    <img className="Disco" src={BlueVault} />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <img className="Disco" src={BlueVault} />
+                </div>
+                <div className="col">
+                    <img className="Disco" src={BlueVault} />
+                </div>
+                <div className="col">
+                    <img className="Disco" src={BlueVault} />
+                </div>
+            </div>
+        </div>
+        <div className="text-center mt-4">
+            <Link className="btn btn-primary" to="/">
+                Return To Home
+            </Link>
+        </div>
     </Layout>
 )
 
